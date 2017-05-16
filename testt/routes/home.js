63db52751f04e7120c3707920146336f66d53b66
minkky <minjiseo95@gmail.com>
2017-05-16 15:37:40 +0900

    testt 폴더 / login

---
+++ testt/routes/home.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var oracledb = require('oracledb');
+var router = express.Router();
+oracledb.autoCommit = true;
+
+var log = {};
+var result = 0;
+
+oracledb.getConnection(
+{
+	user          : "test",
+	password      : "test",
+	connectString : "localhost/orcl"
+},
+function(err, connection){
+	if(err) {console.error(err.message); return; }
+	
+	connection.execute(
+			"select log_id, log_pw from login",
+			{},
+			function(err, result){
+				if(err) {
+					console.error(err.message); return; 
+				}
+
+				log.id = result.rows[0][0];
+				log.pw = result.rows[0][1];
+
+			}
+	);
+});
+
+router.get('/login', function(req, res, next) {
+	res.render('home/login', {id : log.id, pw : log.pw, result : result});
+});
+
+/*router.post('/login', function(req, res, next){
+	console.log("this is post");
+
+	var 
+		user_id = req.body.log_id,
+		user_pw = req.body.log_pw;
+
+	
+	console.log("id : " + log.id + "\n" + "uer : " + user_id);
+	if(log.id == user_id){
+	}
+
+	if( (log.id == user_id) && (log.pw == user_pw)) {
+		console.log("<login>\n"+"SUCCESSS!\n");
+	}
+	else{
+		console.log("<login>\n"+"FAIL!\n");
+		res.render('home/login', {result : 'fail'});
+	}
+
+});
+*/
+module.exports = router;
---
+++ testt/routes/index.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var oracledb = require('oracledb');
+var router = express.Router();
+oracledb.autoCommit = true;
+
+var log = {};
+var result = 0;
+var check = false;
+
+oracledb.getConnection(
+{
+	user          : "db01",
+	password      : "ss2",
+	connectString : "localhost/orcl"
+},
+function(err, connection){
+	if(err) {console.error(err.message); return; }
+	
+	connection.execute(
+			"select log_id, log_pw from login",
+			{},
+			function(err, result){
+				if(err) {
+					console.error(err.message); return; 
+				}
+
+				log.id = result.rows[0][0];
+				log.pw = result.rows[0][1];
+
+			}
+	);
+});
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express', check	: check });
+});
+
+
+
+router.post('/', function(req, res, next){
+	var 
+		user_id = req.body.log_id,
+		user_pw = req.body.log_pw;
+
+	if(log.id == user_id){
+	}
+
+	if( (log.id == user_id) && (log.pw == user_pw)) {
+		console.log("<login>\n"+"SUCCESSS!\n");
+	  	res.render('index', { title: 'Express', check : 'true' });
+	}
+	else{
+		console.log("<login>\n"+"FAIL!\n");
+		res.render('home/login');
+	}
+});
+
+module.exports = router;
