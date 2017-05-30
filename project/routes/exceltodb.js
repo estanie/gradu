c96a15f5a5dd668713d51dd09430a416b042d041
DESKTOP-PKTJRFN\estanie <estanie@naver.com>
2017-05-31 02:21:09 +0900

    들어가는것도 니맘 나오는것도 니맘

--- project/routes/exceltodb.js
+++ project/routes/exceltodb.js
@@ -14,13 +14,28 @@ let worksheet = workbook.Sheets[sheet_name];
 const ALPHA = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 let data = [];
 
+connection.connect(function(err){
+    if (err) {
+        console.error('error connecting: '+err.stack);
+        return;
+    }
+    console.log('connected as id '+connection.threadId);
+});
+
 //첫 줄 출력
 let sql = 'drop table if exists '+filename;
-connection.query(sql,function(err,rows){if(err) throw err;});
+connection.query(sql,function(err,rows){
+    if(err) throw err;
+    console.log(sql+'excuted');
+});
+
 sql = 'create table '+filename+'( 소속 VARCHAR(15), 대학_부서코드 VARCHAR(10),' +
     ' 전공_부서코드 VARCHAR(10), 학번 VARCHAR(10), 입학금 INTEGER,' +
-    ' 수업료 INTEGER, 학점등록 VARCHAR(15) );';
-connection.query(sql,function(err){if(err) throw err;});
+    ' 수업료 INTEGER, 학점등록 VARCHAR(15) )';
+connection.query(sql,function(err){
+    if(err) throw err;
+    console.log(sql+'excuted');
+});
 
 // just header 출력
 for (i = 0;i<7;i++){
@@ -32,7 +47,6 @@ for (i = 0;i<7;i++){
 //나머지 파일들
 for (i = 2;;i++){
     let end = false;
-    {
         for (j = 0;j<7;j++){
             let cell_address;
             let cell;
@@ -49,13 +63,13 @@ for (i = 2;;i++){
             data[j] = cell_value;
 //            console.log(cell_address+' : '+data[j]);
         }
-    }
     if (end)
         break;
     sql = 'INSERT INTO '+filename+' VALUES (?,?,?,?,?,?,?)';
     sql = mysql.format(sql,data);
-    //console.log(sql);
-    connection.query(sql,function(err){if(err) throw err;});
+    connection.query(sql,function(err){
+        if(err) throw err;
+       // console.log(sql+'excuted');
+    });
 }
-
-connection.destroy();
+//connection.destroy();
