6e9f1736542772796b888dadc8816c55accf008c
minkky <minjiseo95@gmail.com>
2017-06-30 00:32:04 +0900

    test

--- project/routes/index.js
+++ project/routes/index.js
@@ -22,55 +22,159 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/baebu', function(req, res, next) {
-  res.render('1baebu');
+  connection.query('select * from baebu', function(err, rows){
+    if (err) {
+      console.error("err : " + err);
+      res.render('1baebu',{title : "none", rows : rows, filename : "파일이 존재하지 않습니다.", err : "err"});
+    }
+
+    console.log("row 잘 받아옴");
+    res.render('1baebu',{title : "baebu", rows : rows, filename : "배부동인량.xlsx", err : "no"});
+  });
 });
 
 router.get('/junlim', function(req, res, next) {
-  res.render('1junlim');
+  connection.query('select * from junlim', function(err, rows){
+    if (err) {
+      console.error("err : " + err);
+      res.render('1junlim',{title : "none", rows : rows, filename : "파일이 존재하지 않습니다.", err : "err"});
+    }
+
+    console.log("row 잘 받아옴");
+    res.render('1junlim',{title : "junlim", rows : rows, filename : "전임교원수.xlsx", err : "no"});
+  });
 });
 
 router.get('/jang', function(req, res, next) {
-  res.render('1jang');
+  connection.query('select * from jang', function(err, rows){
+    if (err) {
+      console.error("err : " + err);
+      res.render('1jang',{title : "none", rows : rows, filename : "파일이 존재하지 않습니다.", err : "err"});
+    }
+
+    console.log("row 잘 받아옴");
+    res.render('1jang',{title : "jang", rows : rows, filename : "장애학생수.xlsx", err : "no"});
+  });
 });
 
 router.get('/hosil', function(req, res, next) {
-  res.render('1hosil');
+  connection.query('select * from hosil', function(err, rows){
+    if (err) {
+      console.error("err : " + err);
+      res.render('1hosil',{title : "none", rows : rows, filename : "파일이 존재하지 않습니다.", err : "err"});
+    }
+
+    console.log("row 잘 받아옴");
+    res.render('1hosil',{title : "jang", rows : rows, filename : "호실 자료 비벗.xlsx", err : "no"});
+  });
 });
 
 router.get('/buseo', function(req, res, next) {
-  res.render('1buseo');
+  connection.query('select * from buseo', function(err, rows){
+    if (err) {
+      console.error("err : " + err);
+      res.render('1buseo',{title : "none", rows : rows, filename : "파일이 존재하지 않습니다.", err : "err"});
+    }
+
+    console.log("row 잘 받아옴");
+    res.render('1buseo',{title : "buseo", rows : rows, filename : "부서코드집.xlsx", err : "no"});
+  });
 });
 
 router.get('/pyeojun', function(req, res, next) {
-  res.render('1pyeojun');
+  connection.query('select * from pyeojun', function(err, rows){
+    if (err) {
+      console.error("err : " + err);
+      res.render('1pyeojun',{title : "none", rows : rows, filename : "파일이 존재하지 않습니다.", err : "err"});
+    }
+
+    console.log("row 잘 받아옴");
+    res.render('1pyeojun',{title : "pyeojun", rows : rows, filename : "표준부서.xlsx", err : "no"});
+  });
 });
 
 router.get('/bonbu', function(req, res, next) {
-  res.render('1bonbu');
+  connection.query('select * from bonbu', function(err, rows){
+    if (err) {
+      console.error("err : " + err);
+      res.render('1bonbu',{title : "none", rows : rows, filename : "파일이 존재하지 않습니다.", err : "err"});
+    }
+
+    console.log("row 잘 받아옴");
+    res.render('1bonbu',{title : "bonbu", rows : rows, filename : "본부부서기준집.xlsx", err : "no"});
+  });
 });
 
 router.get('/jaehak', function(req, res, next) {
-  res.render('1jaehak');
+  connection.query('select * from jaehak', function(err, rows){
+    if (err) {
+      console.error("err : " + err);
+      res.render('1jaehak',{title : "none", rows : rows, filename : "파일이 존재하지 않습니다.", err : "err"});
+    }
+
+    console.log("row 잘 받아옴");
+    res.render('1jaehak',{title : "jaehak", rows : rows, filename : "재학생수.xlsx", err : "no"});
+  });
 });
 
 router.get('/hakbu', function(req, res, next) {
-  res.render('1hakbu');
+  connection.query('select * from hakbu', function(err, rows){
+    if (err) {
+      console.error("err : " + err);
+      res.render('1hakbu',{title : "none", rows : rows, filename : "파일이 존재하지 않습니다.", err : "err"});
+    }
+
+    console.log("row 잘 받아옴");
+    res.render('1hakbu',{title : "hakbu", rows : rows, filename : "학부 개설 과목학점.xlsx", err : "no"});
+  });
 });
 
 router.get('/hakgua', function(req, res, next) {
-  res.render('1hakgua');
+  connection.query('select * from hakgua', function(err, rows){
+    if (err) {
+      console.error("err : " + err);
+      res.render('1hakgua',{title : "none", rows : rows, filename : "파일이 존재하지 않습니다.", err : "err"});
+    }
+
+    console.log("row 잘 받아옴");
+    res.render('1hakgua',{title : "hakgua", rows : rows, filename : "학과집행액.xlsx", err : "no"});
+  });
 });
 
 router.get('/kyojik', function(req, res, next) {
-  res.render('1kyojik');
+  connection.query('select * from kyojik', function(err, rows){
+    if (err) {
+      console.error("err : " + err);
+      res.render('1kyojik',{title : "none", rows : rows, filename : "파일이 존재하지 않습니다.", err : "err"});
+    }
+
+    console.log("row 잘 받아옴");
+    res.render('1kyojik',{title : "kyojik", rows : rows, filename : "교직이수자수.xlsx", err : "no"});
+  });
 });
 
 router.get('/jasan', function(req, res, next) {
-  res.render('1jasan');
+  connection.query('select * from jasan', function(err, rows){
+    if (err) {
+      console.error("err : " + err);
+      res.render('1jasan',{title : "none", rows : rows, filename : "파일이 존재하지 않습니다.", err : "err"});
+    }
+
+    console.log("row 잘 받아옴");
+    res.render('1jasan',{title : "jasan", rows : rows, filename : "자산보유건수.xlsx", err : "no"});
+  });
 });
 
 router.get('/haksang', function(req, res, next) {
-  res.render('1haksang');
+  connection.query('select * from haksang', function(err, rows){
+    if (err) {
+      console.error("err : " + err);
+      res.render('1haksang',{title : "none", rows : rows, filename : "파일이 존재하지 않습니다.", err : "err"});
+    }
+
+    console.log("row 잘 받아옴");
+    res.render('1haksang',{title : "haksang", rows : rows, filename : "학생별 전공선택 현황.xlsx", err : "no"});
+  });
 });
 
 router.get('/plus', function(req, res, next) {
@@ -79,7 +183,7 @@ router.get('/plus', function(req, res, next) {
 
 router.get('/p_hakbu', function(req, res, next) {
   
-  connection.query('select * from hakbu', function(err, rows){
+  connection.query('select * from p_hakbu', function(err, rows){
     if (err) {
       console.error("err : " + err);
       res.render('2hakbu',{title : "none", rows : rows, filename : "파일이 존재하지 않습니다.", err : "err"});
